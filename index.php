323e519a17a0a0fcde579063d25d0b924de30746
yaw owusu <yawowusuofosumanu@gmail.com>
2023-08-12 18:37:18 +0000

    updated

--- index.php
+++ index.php
@@ -44,7 +44,7 @@
       <div class="d-flex align-items-center justify-content-between">
         <a href="index.php" class="logo d-flex align-items-center">
           <img src="img/logo.png" alt="" />
-          <span class="d-none d-lg-block">Swift Car Rentals</span>
+          <span class="d-none d-lg-block">Hospify App</span>
         </a>
         <i class="bi bi-list toggle-sidebar-btn"></i>
       </div>
