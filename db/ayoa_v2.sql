e614f8a0f58a8c99c86204183001c3c286b9c931
David Mensah <david.mensah@hubtel.com>
2023-08-24 17:05:42 +0000

    Add ayao v2 sql

--- db/ayoa_v2.sql
+++ db/ayoa_v2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 24, 2023 at 06:36 PM
+-- Generation Time: Aug 24, 2023 at 07:04 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.1.12
 
@@ -24,35 +24,25 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `doctors`
+-- Table structure for table `nurses`
 --
 
-CREATE TABLE `doctors` (
+CREATE TABLE `nurses` (
   `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
-  `speciality` varchar(100) NOT NULL,
-  `availability` varchar(10) NOT NULL DEFAULT 'YES',
-  `phone_numbers` varchar(20) NOT NULL,
-  `email` varchar(100) NOT NULL
+  `email` varchar(100) NOT NULL,
+  `phone_number` varchar(20) NOT NULL,
+  `department` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `doctors`
---
-
-INSERT INTO `doctors` (`id`, `name`, `speciality`, `availability`, `phone_numbers`, `email`) VALUES
-(1, 'Nana Yaw', 'Surgery ', 'YES', '0245931671', 'nanayaw@email.com'),
-(2, 'Pendy', 'Surgery ', 'YES', '0558157667', 'pendy@email.com'),
-(3, 'David Mensah', 'Surgery ', 'YES', '0558157666', 'davidmensah@email.com');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `doctors`
+-- Indexes for table `nurses`
 --
-ALTER TABLE `doctors`
+ALTER TABLE `nurses`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -60,10 +50,10 @@ ALTER TABLE `doctors`
 --
 
 --
--- AUTO_INCREMENT for table `doctors`
+-- AUTO_INCREMENT for table `nurses`
 --
-ALTER TABLE `doctors`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+ALTER TABLE `nurses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
