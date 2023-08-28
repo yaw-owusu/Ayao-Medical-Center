42645791de7a25608b90e30ee17f7ac2f811c217
David Mensah <david.mensah@hubtel.com>
2023-08-28 23:17:38 +0000

    Add initial controller action for home

--- index.php
+++ index.php
@@ -1,6 +1,23 @@
 <?php 
 
+$action = filter_input(INPUT_POST, 'action');
 
+if ($action == NULL) {
+    $action = filter_input(INPUT_GET, 'action');
+    if ($action == NULL) {
+        $action = 'home';
+    }
+}
+
+switch ($action) {
+    case 'home':
+        include './dashboard.php';
+        break;
+    
+    default:
+        # code...
+        break;
+}
 
 
 ?>
