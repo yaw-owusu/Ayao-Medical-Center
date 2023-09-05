a30fec783af6f70699e8b645d082f6610e305596
David Mensah <david.mensah@hubtel.com>
2023-09-05 01:00:31 +0000

    add create supporter, delete supporter methods

--- model/supports.php
+++ model/supports.php
@@ -15,34 +15,27 @@ function get_all_supports(){
 }
 
 
-
+// Get supporter by id
 function get_support($id){
     global $db;
 
-    $sql = "SELECT * FROM admin WHERE type = 'support' AND id = :id";
+    $sql = "SELECT * FROM supports WHERE id = :id";
 
     $statement = $db->prepare($sql);
     $statement->bindValue(':id', $id);
     $statement->execute();
 
-    $selected_marketer = $statement->fetch();
+    $selected_supporter = $statement->fetch();
     $statement->closeCursor();
 
-    if( $selected_marketer == null ){
+    if( $selected_supporter == null ){
         return false;
-    }else{
-        
-        return $selected_marketer;
-        
     }
 
-
+    return $selected_supporter;
 }
 
-
-
 // Get number of supports
-
 function get_supports_count(){
     global $db;
 
@@ -52,6 +45,51 @@ function get_supports_count(){
 
     return $supports->rowCount();
 
+}
+
+// Delete supporter
+function delete_supporter($id) {
+
+    global $db;  
+    
+    $sql = "DELETE FROM supports WHERE id = :id";
+    
+    $statement = $db->prepare($sql);
+    $statement->bindValue(':id', $id);
+    $success = $statement->execute();
+    $statement->closeCursor();
+
+    return $success;
+}
+
+function create_supporter($name,$email, $password,$image) {
+
+    global $db;  
+    
+    $sql = "INSERT INTO supporters (name, email, password, type,date,image) 
+    VALUES (:name,:email,:password,:type,CURRENT_DATE(),:image)";
+
+
+    $statement = $db->prepare($sql);
+    $statement->bindValue(':name', $name);
+    $statement->bindValue(':email', $email);
+    $statement->bindValue(':password', $password);
+    $statement->bindValue(':type', 'Marketer');
+    $statement->bindValue(':image', $image["name"]);
+    
+    
+
+    $success = $statement->execute();
+    $statement->closeCursor();
+
+    $marketer_id = $db->lastInsertId();
+
+    if($success == true){
+        upload_admin_image($image,"marketer",$marketer_id);
+        return $marketer_id;
+    }else{
+        return false;
+    }
 }
 
 
